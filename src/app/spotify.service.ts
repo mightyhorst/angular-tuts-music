a6fa86f1615f59003e23a2e45a6d3fda6ff63ce0
Nick Mitchell <nick@kitset.io>
2018-09-06 12:55:04 +1000

    Init commit

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import {
   Http,
   Headers,
@@ -6,6 +7,9 @@ import {
 } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import {throwError} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 
 /**
@@ -38,7 +42,35 @@ export class SpotifyService {
     return this.http
       .request(queryURL, options)
       .map((res: any) => res.json());
+
+    // return this.http
+    //   .request(queryURL, options).pipe(
+    //     map(res => res.json() ),
+    //     catchError(this.handleError)
+    //   );
   }
+
+  // private handleError(error: HttpErrorResponse) {
+  //   if (error.error instanceof ErrorEvent) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error.message);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.error(
+  //       `Backend returned code ${error.status}, ` +
+  //       `body was: ${error.error}`);
+
+
+  //     if(confirm('Hack - go here to get an OAuth https://developer.spotify.com/console/get-search-item/')){
+  //       window.location.href='https://developer.spotify.com/console/get-search-item/'
+  //     }
+
+
+  //   }
+  //   // return an observable with a user-facing error message
+  //   throwError('Something bad happened; please try again later.');
+  // };
 
   search(query: string, type: string): Observable<any[]> {
     return this.query(`/search`, [
--- src/environments/spotifyApiKey.ts
+++ src/environments/spotifyApiKey.ts
@@ -1,6 +1,6 @@
 //export const SpotifyAPIKey = 'BQBB-kLnibXFvQ04OeBi1pAxYY-MVWQ-Wz0m4X20uDliYan9Ljxj-DwmkMklcC4th8x-EXlEh3NVGj4A7AI';
 
 /**
-* HAck - go here to get an OAuth https://developer.spotify.com/console/get-search-item/?q=Muse&type=&market=&limit=&offset=
+* Hack - go here to get an OAuth https://developer.spotify.com/console/get-search-item/
 **/
 export const SpotifyAPIKey = 'BQAPiI6Tv5Nt-7tT_KAU1g6Z0d2t4rlyeBEiYmNPQ2QzHZAyW_t0M1t4AkiBglMr0tXufRGHSHrnBSyrA_cwUqfq_GKZz5KwCNFPkmczGz0T1L_WY30Im4dIMmaI8_YUJD0-outLE7fdJds6vMdOldJHW-2WaXfnvyxBGRrbGAjYxJZvCnKgjOSI_z2OHBXXtfZo0suMIriET94L1Gd_RHgvLbNXVCXU8ejQLfLeFUpkor7g6eAaH3tOU6z40o_dhLXfjlrs3Ge3AQ';
